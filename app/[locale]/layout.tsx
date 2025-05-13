49200e72c099338f1219b6feb4519321a952afb1
Dawit Tesfaye <tdawit523@gmail.com>
2025-05-13 11:35:14 +0300

    little change

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -13,22 +13,23 @@ export const metadata: Metadata = {
   generator: "Next.js",
   applicationName: "debrat ena bealat",
 };
+
 export default async function RootLayout({
   children,
-  // params: { locale },
-}: Readonly<{
+}: // params: { locale },
+Readonly<{
   children: React.ReactNode;
   // params: { locale: string };
-
 }>) {
-  const locale = await getLocale()
+  const locale = await getLocale();
   return (
     <html lang={locale}>
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
-        <NextIntlClientProvider >{children}</NextIntlClientProvider>
+        
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
     </html>
   );
